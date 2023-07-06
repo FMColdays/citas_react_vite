04899e464dcafb00a178d65449d62f360948cbda
Erick <erickgp51@gmail.com>
2023-07-06 16:09:02 -0600

    cambio

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <h1 className="titulo-header">Seguimiento Pacientes Para <span>Veterinarias</span> </h1>
+    <h1 className="titulo-header">Seguimiento Pacientes <span>Veterinarias</span> </h1>
   )
 }
 
